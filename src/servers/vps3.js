33e32cdeae59cc415a564b2b6df3134a88c8a2bc
Roald Hacquebord <roald@hacquebordbuckles.nl>
2016-12-29 23:25:42 +0100

    Add dir to store ssh keys

--- src/servers/vps3.js
+++ src/servers/vps3.js
@@ -34,8 +34,7 @@ const vps3 = Object.assign(server, {
   ssh: {
     host: process.env.VPS3_HOST,
     username: process.env.VPS3_USERNAME,
-    privateKey: process.env.VPS3_PRIVATE_KEY,
-    passphrase: process.env.VPS3_PASSPHRASE
+    privateKey: process.env.VPS3_PRIVATE_KEY
   },
   actions: [
     createTmpDir,
