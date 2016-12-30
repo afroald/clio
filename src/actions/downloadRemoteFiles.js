a06bda16ff8d01427504cb045d2e7fd48b565362
Roald Hacquebord <roald@hacquebordbuckles.nl>
2016-12-30 16:16:57 +0100

    Remove unneeded import

--- src/actions/downloadRemoteFiles.js
+++ src/actions/downloadRemoteFiles.js
@@ -1,8 +1,6 @@
 const path = require('path');
 const u = require('updeep');
 
-const CommandFailedError = require('../errors/CommandFailedError');
-
 async function downloadRemoteFiles(backup, connection) {
   const downloadedFiles = [];
 
