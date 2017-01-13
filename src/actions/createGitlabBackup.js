da7dba6d1b459974d61d56d1b9f26a3828bbee72
Roald Hacquebord <roald@hacquebordbuckles.nl>
2017-01-13 14:36:18 +0100

    Add shortcut for action

--- src/actions/createGitlabBackup.js
+++ src/actions/createGitlabBackup.js
@@ -2,13 +2,13 @@ const path = require('path');
 const u = require('updeep');
 
 const execRemoteCommand = require('../execRemoteCommand');
-const action = require('../action/action');
+const action = require('../action');
 
 const gitlabBackupPath = '/var/opt/gitlab/backups';
 
 module.exports = u({
   title: 'Create Gitlab Backup',
-  action: u.constant(async (backup, connection) => {
+  action: () => async function createGitlabBackup(backup, connection) {
     const command = await execRemoteCommand(connection, 'sudo gitlab-rake gitlab:backup:create');
 
     const matches = command.stdout.match(/^Creating backup archive: (.*\.tar) \.\.\. done$/m);
@@ -24,5 +24,5 @@ module.exports = u({
         ])
       }
     }, backup);
-  })
+  }
 }, action);
