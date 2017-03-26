f429ea28ec58562b682435295bd3c1f17add7b6a
Roald Hacquebord <roald@hacquebordbuckles.nl>
2017-03-26 16:03:15 +0200

    Add default config

---
+++ src/config/defaultConfig.js
@@ -0,0 +1,15 @@
+const { freeze } = require('updeep');
+const path = require('path');
+
+const config = freeze({
+  paths: {
+    config: path.resolve('/etc/clio'),
+    tmp: path.resolve('/tmp'),
+    storage: path.resolve('/var/backups')
+  },
+  gpg: {
+    recipient: null
+  }
+});
+
+module.exports = config;
